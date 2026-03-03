d231e4d9e88192c9a30d4e0d3876dc1b87b9ec50
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-03 12:15:00 +0530

    Feature: Created App Context

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AppProvider } from "./context/AppContext.tsx";
 
 export const authService = "http://localhost:4000";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
       <Toaster />
     </GoogleOAuthProvider>
   </StrictMode>,
