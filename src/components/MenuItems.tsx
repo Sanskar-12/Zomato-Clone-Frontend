8da1a7dc3978d9335d67a33249999bfafb233ab9
Sanskar-12 <sanskarv2004@gmail.com>
2026-03-22 12:01:52 +0530

    Feature: Added delete item and update status of item functionality

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import type { IMenuItem } from "../types";
-import { BsEye } from "react-icons/bs";
+import { BsCart, BsEye } from "react-icons/bs";
 import { FiEyeOff } from "react-icons/fi";
 import { BiTrash } from "react-icons/bi";
+import { VscLoading } from "react-icons/vsc";
+import axios from "axios";
+import { restaurantService } from "../main";
+import toast from "react-hot-toast";
 
 interface MenuItemsProps {
   items: IMenuItem[];
@@ -12,6 +16,44 @@ interface MenuItemsProps {
 
 const MenuItems = ({ items, onItemDeleted, isSeller }: MenuItemsProps) => {
   const [loadingItemId, setLoadingItemId] = useState<string | null>(null);
+
+  const handleDelete = async (itemId: string) => {
+    const confirm = window.confirm("Are you sure you want to delete this item");
+    if (!confirm) return;
+
+    try {
+      await axios.delete(`${restaurantService}/api/item/${itemId}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      toast.success("Item deleted successfully");
+      onItemDeleted();
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete item");
+    }
+  };
+
+  const toggleAvailability = async (itemId: string) => {
+    try {
+      const { data } = await axios.get(
+        `${restaurantService}/api/item/status/${itemId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+
+      toast.success(data.message);
+      onItemDeleted();
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to toggle availability item");
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -47,12 +89,12 @@ const MenuItems = ({ items, onItemDeleted, isSeller }: MenuItemsProps) => {
                     </p>
                   )}
                 </div>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-between">
                   <p className="font-medium">₹{item.price}</p>
                   {isSeller && (
                     <div className="flex gap-2">
                       <button
-                        onClick={() => {}}
+                        onClick={() => toggleAvailability(item?._id)}
                         className="rounded-lg p-2 text-gray-600 hover:bg-gray-100"
                       >
                         {item.isAvailable ? (
@@ -62,12 +104,29 @@ const MenuItems = ({ items, onItemDeleted, isSeller }: MenuItemsProps) => {
                         )}
                       </button>
                       <button
-                        onClick={() => {}}
+                        onClick={() => handleDelete(item?._id)}
                         className="rounded-lg p-2 text-red-500 hover:bg-red-50"
                       >
                         <BiTrash size={18} />
                       </button>
                     </div>
+                  )}
+                  {!isSeller && (
+                    <button
+                      disabled={!item.isAvailable || isLoading}
+                      onClick={() => {}}
+                      className={`flex items-center justify-center rounded-lg p-2 ${
+                        !item.isAvailable || isLoading
+                          ? "cursor-not-allowed text-gray-400"
+                          : "text-red-500 hover:bg-red-50"
+                      }`}
+                    >
+                      {isLoading ? (
+                        <VscLoading size={18} className="animate-spin" />
+                      ) : (
+                        <BsCart size={18} />
+                      )}
+                    </button>
                   )}
                 </div>
               </div>
